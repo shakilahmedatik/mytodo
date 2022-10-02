98b588aedb8197356e49eeea7f81467c6cc01424
Shakil Ahmed Atik <shakilatik@gmail.com>
2022-10-03 03:06:37 +0600

    style issue fixed

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -15,13 +15,20 @@ const GoalInput = ({ addInputHandler, visible }) => {
           style={styles.textInput}
           placeholder='Your course goal!'
         />
-        <Button
-          onPress={() => {
-            addInputHandler(enteredGoalText)
-            setEnteredGoalText('')
-          }}
-          title='Add Goal'
-        />
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button
+              onPress={() => {
+                addInputHandler(enteredGoalText)
+                setEnteredGoalText('')
+              }}
+              title='Add Goal'
+            />
+          </View>
+          <View style={styles.button}>
+            <Button title='Cancel' />
+          </View>
+        </View>
       </View>
     </Modal>
   )
@@ -32,18 +39,25 @@ export default GoalInput
 const styles = StyleSheet.create({
   inputContainer: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    justifyContent: 'center',
     alignItems: 'center',
     marginBottom: 24,
     borderBottomWidth: 1,
     borderColor: '#cccccc',
+    padding: 16,
   },
   textInput: {
     borderWidth: 1,
     borderColor: '#cccccc',
-    width: '70%',
-    marginRight: 8,
+    width: '100%',
     padding: 8,
+  },
+  buttonContainer: {
+    marginTop: 16,
+    flexDirection: 'row',
+  },
+  button: {
+    width: 100,
+    marginHorizontal: 8,
   },
 })
